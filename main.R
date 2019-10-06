eb7bf4d5081f0399f7d8ed1259ae55b71e3ed27b
Ola Silgjerd <36745349+silgjerd@users.noreply.github.com>
2019-10-06 21:27:41 +0200

    Add files via upload

--- main.R
+++ main.R
@@ -14,50 +14,68 @@ library(outliers)
 library(mltools)
 library(onehot)
 library(BBmisc)
+library(scales)
 graphics.off() #reset graphics
 rm(list = ls()) #clear workspace, variables
 options(scipen=999) #disable scientific notation
 
 # Import data
-data <- read_delim("pd_data_v2.csv",
-                   ";", escape_double = FALSE,
-                   col_types = cols(default = col_factor(levels = c("0", "1"))),
-                   trim_ws = TRUE)
+# data <- read_delim("pd_data_v2.csv",
+#                    ";", escape_double = FALSE,
+#                    col_types = cols(default = col_factor(levels = c("0", "1"))),
+#                    trim_ws = TRUE)
+
+data <- read.csv("pd_data_v2.csv", sep = ";")
+data$default <- as.factor(data$default)
 
 
+# Preprocessing
+
+# Outlier treatment
+data_outliers <- data %>%
+  filter_all(any_vars(. %in% c(-1e19, 1e19))) #filtering error values
+
+data_filtered <- setdiff(data, data_outliers)
 
 data %>%
   group_by(default) %>% tally()
 
-
-# Outlier treatment
-data_outliers <- data %>%
-  filter_all(any_vars(. %in% c(-1e19, 1e19))) #filtering outliers
-
-data_filtered <- setdiff(data, data_outliers)
-
 data_filtered %>%
   group_by(default) %>% tally()
 
+# Feature engineering
 
-normalize1 <- function(x){(x-min(x))/(max(x)-min(x))}
+# Creating dummies
+data_filtered <- data_filtered %>% #dummy variables, 1 if negative, 0 if positive
+  mutate(d_pm_neg = if_else(profit_margin < 0, 1, 0)) %>%
+  mutate(d_om_neg = if_else(operating_margin < 0, 1, 0)) %>%
+  mutate(d_em_neg = if_else(EBITDA_margin < 0, 1, 0))
 
-for (i in 2:24) {data_normalized[,i] <- normalize1(data_filtered[,i])}
 
-data_normalized <- data_filtered
-data_normalized[,2] <- normalize1(data_filtered[,2])
+
+
 
 
 
 # Train test split
 set.seed(123)
-train.index <- createDataPartition(data$default, p = .7, list = FALSE) #stratified sampling
+train.index <- createDataPartition(data$default, p = .75, list = FALSE) #stratified sampling
 train <- data[ train.index,]
 test  <- data[-train.index,]
 
 
+quantiles <- tibble()
+for (i in 1:ncol(train)) {
+  
+  
+  
+  
+}
 
 
+quantiles <- bind_rows(quantiles, c("low" = quantile(data_filtered[,2], probs = c(.05, .95))[[1]],
+                                    "high" = quantile(data_filtered[,2], probs = c(.05, .95))[[2]]))
+min(Winsorize(data_filtered$profit_margin, probs = c(.05, .95)))
 
 
 
