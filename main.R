71e2d79caa497b56c35f4b393e983ba08f50d91d
Ola Silgjerd <36745349+silgjerd@users.noreply.github.com>
2019-10-07 15:47:47 +0200

    Add files via upload

--- main.R
+++ main.R
@@ -10,32 +10,36 @@ library(lmtest)
 library(DescTools)
 library(readxl)
 library(caret)
+library(e1071)
 library(outliers)
 library(mltools)
 library(onehot)
 library(BBmisc)
 library(scales)
+library(ROCR)
+library(pROC)
+library(xgboost)
 graphics.off() #reset graphics
 rm(list = ls()) #clear workspace, variables
 options(scipen=999) #disable scientific notation
 
 # Import data
-# data <- read_delim("pd_data_v2.csv",
-#                    ";", escape_double = FALSE,
-#                    col_types = cols(default = col_factor(levels = c("0", "1"))),
-#                    trim_ws = TRUE)
-
 data <- read.csv("pd_data_v2.csv", sep = ";")
+
 data$default <- as.factor(data$default)
+data$industry <- as.factor(data$industry)
 
 
 # Preprocessing
 
-# Outlier treatment
+# Bad data
 data_outliers <- data %>%
   filter_all(any_vars(. %in% c(-1e19, 1e19))) #filtering error values
+data_filtered <- setdiff(data, data_outliers) #removing values
 
-data_filtered <- setdiff(data, data_outliers)
+data_filtered <- subset(data_filtered, total_assets >= 0) #removing impossible bad observations
+
+
 
 data %>%
   group_by(default) %>% tally()
@@ -51,6 +55,17 @@ data_filtered <- data_filtered %>% #dummy variables, 1 if negative, 0 if positiv
   mutate(d_om_neg = if_else(operating_margin < 0, 1, 0)) %>%
   mutate(d_em_neg = if_else(EBITDA_margin < 0, 1, 0))
 
+# One hot encoding industry variable
+industry_dmy <- dummyVars(" ~ industry", data = data_filtered)
+industry_trsf <- data.frame(predict(industry_dmy, newdata = data_filtered)) #making new one hot encoded variables for each industry
+data_filtered <- bind_cols(data_filtered, industry_trsf) #appending new variables
+data_filtered <- data_filtered[ , !(names(data_filtered) %in% c("industry"))] #drop original indutry column
+
+
+
+
+
+
 
 
 
@@ -59,23 +74,56 @@ data_filtered <- data_filtered %>% #dummy variables, 1 if negative, 0 if positiv
 
 # Train test split
 set.seed(123)
-train.index <- createDataPartition(data$default, p = .75, list = FALSE) #stratified sampling
-train <- data[ train.index,]
-test  <- data[-train.index,]
+train.index <- createDataPartition(data_filtered$default, p = .75, list = FALSE) #stratified sampling
+train <- data_filtered[ train.index,]
+test  <- data_filtered[-train.index,]
 
 
-quantiles <- tibble()
-for (i in 1:ncol(train)) {
-  
-  
-  
-  
+# Winsorizing (capping)
+wins_cols <- c(2,4:14,17,19,20,23) #columns to apply capping
+for (i in wins_cols) {
+  qs <- quantile(train[,i], probs = c(.05, .95)) #top and bottom quantiles
+  train[,i] <- Winsorize(train[,i], minval = qs[1], maxval = qs[2]) #apply train data quantiles to both train and test data to avoid bias during cross validation
+  test[,i]  <- Winsorize(test[,i],  minval = qs[1], maxval = qs[2])
 }
 
+# Balancing training set (downsampling / equal size sampling)
+train <- downSample(train, train$default)
 
-quantiles <- bind_rows(quantiles, c("low" = quantile(data_filtered[,2], probs = c(.05, .95))[[1]],
-                                    "high" = quantile(data_filtered[,2], probs = c(.05, .95))[[2]]))
-min(Winsorize(data_filtered$profit_margin, probs = c(.05, .95)))
 
+
+# Modelling
+
+#Logistic regression
+m_logit <- glm(default ~ cost_of_debt + age_of_company + adverse_audit_opinion,
+               data = train,
+               family = binomial)
+
+#XGBoost
+m_xgb <- xgboost(data = , max.depth = 2, eta = 1, nthread = 2, nrounds = 2, objective = "binary:logistic", verbose = 1)
+
+
+#Random forest
+m_rand <- 
+
+
+# Predictions
+preds <- tibble("Logit" = predict(m_logit, test, type = "response"))
+
+preds <- as.factor(if_else(preds > 0.5, 1, 0))
+
+confusionMatrix(preds, test$default)
+
+rocplot <- function(pred, truth, ...) {
+  predob = prediction(pred, truth)
+  perf = performance(predob, "tpr", "fpr")
+  plot(perf, ...)
+  area <- auc(truth, pred)
+  area <- format(round(area, 4), nsmall = 4)
+  text(x=0.8, y=0.1, labels = paste("AUC =", area))
+  segments(x0=0, y0=0, x1=1, y1=1, col="gray", lty=2)
+}
+
+rocplot(preds, test$default, col="blue")
 
 
